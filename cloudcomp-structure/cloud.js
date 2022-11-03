b712e6c3ed177a23e15d537f718d36177d74e698
Mark Pawlaczyk <mark@scapesketch.com>
2022-11-03 15:47:42 -0400

    Add files via upload

---
+++ cloudcomp-structure/cloud.js
@@ -0,0 +1,33 @@
+{
+    "version": "1.7.3",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 331864,
+    "boundingBox": {
+        "lx": -35.785484313964847,
+        "ly": -44.50516128540039,
+        "lz": -1.5163215398788453,
+        "ux": -3.485656976699829,
+        "uy": -12.205333948135376,
+        "uz": 30.78350579738617
+    },
+    "tightBoundingBox": {
+        "lx": -35.7854843022972,
+        "ly": -44.505161282287108,
+        "lz": -1.5143010616514999,
+        "ux": -3.4865794266546006,
+        "uy": -12.320823658840404,
+        "uz": 9.819991109574
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "CLASSIFICATION",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.27972471714019778,
+    "scale": 0.001,
+    "hierarchyStepSize": 5,
+    "pointClassCount": {
+        "0": 331864
+    }
+}

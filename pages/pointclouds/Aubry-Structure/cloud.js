c00d1e488e8225bf7df4a2b4b98c99d13bd3f8ff
Mark Pawlaczyk <mark@scapesketch.com>
2022-11-03 17:57:54 -0400

    Uploaded new folder

---
+++ pages/pointclouds/Aubry-Structure/cloud.js
@@ -0,0 +1,67 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 331866,
+    "boundingBox": {
+        "lx": -35.785484313964847,
+        "ly": -44.50516128540039,
+        "lz": -1.5163215398788453,
+        "ux": -3.485656976699829,
+        "uy": -12.205333948135376,
+        "uz": 30.78350579738617
+    },
+    "tightBoundingBox": {
+        "lx": -35.7854843022972,
+        "ly": -44.505161282287108,
+        "lz": -1.5163215417368,
+        "ux": -3.485656976666398,
+        "uy": -12.320823658840404,
+        "uz": 9.819991109574
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.27972471714019778,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
